70279b6ccc75b04abe171cdfe7191f9632c55c80
Hayden Bleasel <hello@haydenbleasel.com>
2025-10-11 00:44:53 -0700

    Update results.client.tsx

--- components/results.client.tsx
+++ components/results.client.tsx
@@ -1,12 +1,20 @@
 "use client";
 
 import type { ListBlobResult } from "@vercel/blob";
-import { ArrowLeftIcon, Loader2Icon } from "lucide-react";
+import {
+  ArrowLeftIcon,
+  FileIcon,
+  ImageIcon,
+  ImageUpIcon,
+  Loader2Icon,
+  UploadIcon,
+} from "lucide-react";
 import { useActionState, useEffect } from "react";
 import { toast } from "sonner";
 import { search } from "@/app/actions/search";
 import { Preview } from "./preview";
 import { Button } from "./ui/button";
+import { Empty, EmptyDescription, EmptyHeader, EmptyTitle } from "./ui/empty";
 import { Input } from "./ui/input";
 import { UploadButton } from "./upload-button";
 import { useUploadedImages } from "./uploaded-images-provider";
@@ -33,30 +41,56 @@ export const ResultsClient = ({ defaultData }: ResultsClientProps) => {
 
   return (
     <>
-      <div className="columns-3 gap-4">
-        {images.map((image, index) => (
-          <Preview
-            key={image.url}
-            priority={index < PRIORITY_COUNT}
-            url={image.url}
-          />
-        ))}
-        {"data" in state && state.data?.length
-          ? state.data.map((blob, index) => (
-              <Preview
-                key={blob.url}
-                priority={index < PRIORITY_COUNT}
-                url={blob.url}
-              />
-            ))
-          : defaultData.map((blob, index) => (
-              <Preview
-                key={blob.url}
-                priority={index < PRIORITY_COUNT}
-                url={blob.downloadUrl}
-              />
-            ))}
-      </div>
+      {images.length ||
+      defaultData.length ||
+      ("data" in state && state.data?.length) ? (
+        <div className="columns-3 gap-4">
+          {images.map((image, index) => (
+            <Preview
+              key={image.url}
+              priority={index < PRIORITY_COUNT}
+              url={image.url}
+            />
+          ))}
+          {"data" in state && state.data?.length
+            ? state.data.map((blob, index) => (
+                <Preview
+                  key={blob.url}
+                  priority={index < PRIORITY_COUNT}
+                  url={blob.url}
+                />
+              ))
+            : defaultData.map((blob, index) => (
+                <Preview
+                  key={blob.url}
+                  priority={index < PRIORITY_COUNT}
+                  url={blob.downloadUrl}
+                />
+              ))}
+        </div>
+      ) : (
+        <Empty className="h-full min-h-[50vh] rounded-lg border">
+          <EmptyHeader className="max-w-none">
+            <div className="relative isolate mb-8 flex">
+              <div className="-rotate-12 translate-x-2 translate-y-2 rounded-full border bg-background p-3 shadow-xs">
+                <ImageIcon className="size-5 text-muted-foreground" />
+              </div>
+              <div className="z-10 rounded-full border bg-background p-3 shadow-xs">
+                <UploadIcon className="size-5 text-muted-foreground" />
+              </div>
+              <div className="-translate-x-2 translate-y-2 rotate-12 rounded-full border bg-background p-3 shadow-xs">
+                <FileIcon className="size-5 text-muted-foreground" />
+              </div>
+            </div>
+            <EmptyTitle>No images found</EmptyTitle>
+            <EmptyDescription>
+              Upload some images with the{" "}
+              <ImageUpIcon className="inline size-4" /> button below to get
+              started!
+            </EmptyDescription>
+          </EmptyHeader>
+        </Empty>
+      )}
 
       <form
         action={formAction}
